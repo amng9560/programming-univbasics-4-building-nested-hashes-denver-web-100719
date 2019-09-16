d0041540afa5adaf66b0c6692dc14f196f70c646
Amy Nguyen <amy.nguyen730@gmail.com>
2019-09-16 21:24:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,8 +2,8 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	monopoly = {
-		railroad: {}
-	}
+	railroads: {}
+}
 end
 
 def monopoly_with_second_tier
