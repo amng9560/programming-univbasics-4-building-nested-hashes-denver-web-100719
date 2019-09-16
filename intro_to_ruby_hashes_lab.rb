a1846652c15837a4e16804a2854fea3f85edd961
Amy Nguyen <amy.nguyen730@gmail.com>
2019-09-16 21:21:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,10 +29,10 @@ def monopoly_with_third_tier
 		:railroad => {
 			pieces: 4,
 			name: {
-				reading_railroad:{},
-				pennsylvania: {},
-				b_and_o_railroad: {},
-				shortline: {}
+				:reading_railroad => {},
+				:pennsylvania => {},
+				:b_and_o_railroad => {},
+				:shortline => {}
 			},
 			rent_in_dollars: {one_piece_owned: 25, two_piece_owned: 50, three_pieces_owned: 100, four_pieces_owned:200}
 		}
