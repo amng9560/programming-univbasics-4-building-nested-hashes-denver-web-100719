c293ab68d3f90a05b18c317d245fa1c06d14cba4
Amy Nguyen <amy.nguyen730@gmail.com>
2019-09-16 21:23:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,10 +48,10 @@ def monopoly_with_fourth_tier
 		railroad: {
 			pieces: 4,
 			name: {
-				reading_railroad:{"mortage value" => "$100"},
-				pennsylvania: {"mortage value" => "$200"},
-				b_and_o_railroad: {"mortage value" => "$400"},
-				shortline: {"mortage value" => "$800"}
+				:reading_railroad => {"mortage value" => "$100"},
+				:pennsylvania => {"mortage value" => "$200"},
+				:b_and_o_railroad => {"mortage value" => "$400"},
+				:shortline => {"mortage value" => "$800"}
 			},
 			rent_in_dollars: {one_piece_owned: 25, two_piece_owned: 50, three_pieces_owned: 100, four_pieces_owned:200}
 		}
