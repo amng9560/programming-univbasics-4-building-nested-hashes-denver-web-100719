6a8c2ede0217a1dc0cd2a2f3fdb1cca698365340
Amy Nguyen <amy.nguyen730@gmail.com>
2019-09-16 21:26:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,7 @@ def monopoly_with_third_tier
 				:b_and_o_railroad => {},
 				:shortline => {}
 			},
-			rent_in_dollars: {one_piece_owned: 25, two_piece_owned: 50, three_pieces_owned: 100, four_pieces_owned:200}
+			rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned:200}
 		}
 	}
 end
@@ -53,7 +53,7 @@ def monopoly_with_fourth_tier
 				:b_and_o_railroad => {"mortage value" => "$400"},
 				:shortline => {"mortage value" => "$800"}
 			},
-			rent_in_dollars: {one_piece_owned: 25, two_piece_owned: 50, three_pieces_owned: 100, four_pieces_owned:200}
+			rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned:200}
 		}
 	}
 end
